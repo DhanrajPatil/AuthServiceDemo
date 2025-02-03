3500623689c09f24916ce4d2e292a7b5b8a007f8
Dhanraj Patil <dhanraj.patil@hotmail.com>
2025-02-03 13:43:32 +0530

    Initial Set up for Authentication Microservice

---
+++ src/test/java/com/elitefolk/authservicedemo/AuthServiceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elitefolk.authservicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
